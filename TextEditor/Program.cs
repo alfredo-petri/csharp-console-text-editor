a4a16d2c31f5abcb51f1be5c2a852a0c42f56ce0
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-14 20:47:20 -0300

    feat: add menu method

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("");
+            Console.WriteLine("0 - Sair da aplicação");
+            Console.WriteLine("1 - Abrir um arquivo");
+            Console.WriteLine("3 - Criar um arquivo");
+            Console.WriteLine("");
+            Console.Write("Selecione a opção desejada: ");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: Console.WriteLine("saiu da aplicação"); break;
+                case 1: Console.WriteLine("abre um arquivo"); break;
+                case 2: Console.WriteLine("cria um arquivo"); break;
+                default: Console.WriteLine("chama o menu novamente"); break;
+            }
+        }
+    }
+}
